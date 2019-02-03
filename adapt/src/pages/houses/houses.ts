6f0d4877d3f760e59a5bfbdbfa6bd7ce81759039
Rebeca <rmb0047@auburn.edu>
2019-02-03 15:56:55 -0600

    The rest of the files

---
+++ adapt/src/pages/houses/houses.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-houses',
+  templateUrl: 'houses.html'
+})
+export class HousesPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ adapt/src/pages/residents/residents.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-residents',
+  templateUrl: 'residents.html'
+})
+export class ResidentsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
